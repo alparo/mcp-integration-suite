617bdc5dcc833d4c72b515263b28673873412433
Noah Buchmann <noah.buchmann@contriboot.de>
2025-04-01 13:01:38 +0200

    add iflow example handler in server

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -2,9 +2,11 @@ import { McpServer } from "@modelcontextprotocol/sdk/server/mcp";
 import { registerPackageHandlers } from "./packages";
 import { registerIflowHandlers } from "./iflow/tools";
 import { registerPackageDiscoverHandler } from "./discover/packages";
+import { registerIflowExampleHandler } from "./iflow/exmaples";
 
 export const registerAllHandlers = (server: McpServer) => {
 	registerPackageHandlers(server);
 	registerIflowHandlers(server);
 	registerPackageDiscoverHandler(server);
+	registerIflowExampleHandler(server);
 };

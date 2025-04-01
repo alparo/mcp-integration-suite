0e315abd55dde9a058fb8b9bee3f61b03736d804
Noah Buchmann <noah.buchmann@contriboot.de>
2025-04-01 13:01:49 +0200

    fix json related errors

--- src/handlers/iflow/tools.ts
+++ src/handlers/iflow/tools.ts
@@ -2,6 +2,7 @@ import { McpServer } from "@modelcontextprotocol/sdk/server/mcp";
 import { z } from "zod";
 import { createIflow, deployIflow, getIflow, saveAsNewVersion, updateIflow } from "../../api/iflow";
 import { logError, logInfo } from "../..";
+import { addDoublequotes, doublequotes, escapeDoublequotes } from "../../utils/specialchars";
 
 export const updateIflowFiles = z.array(
 	z.object({
@@ -10,22 +11,29 @@ export const updateIflowFiles = z.array(
 			.describe(
 				'filepath within project. E.g. "resources/scenarioflows/integrationflow/myiflow.iflw'
 			),
-		content: z.string().describe("File content"),
+		content: z.string().describe(`File content. Use ${doublequotes} for a single backslash to avoid parsing errors `),
 	})
 );
 
 export const registerIflowHandlers = (server: McpServer) => {
 	server.tool(
 		"get-iflow",
-		"Get the data of an iflow and the contained ressources. Some ressources might relay on other package artefacts which are not included but reffrenced",
+		`Get the data of an iflow and the contained ressources. 
+Doublequotes are replaced with ${doublequotes}
+Some ressources might relay on other package artefacts which are not included but reffrenced
+doublequotes are replaced by 
+`,
 		{
 			id: z.string().describe("ID of the IFLOW"),
 		},
 		async ({ id }) => {
 			logInfo(`trying to get iflow ${id}`);
 			try {
+				const fileContent = await getIflow(id);
+				const escapedFileContent = escapeDoublequotes(fileContent);
+				
 				return {
-					content: [{ type: "text", text: await getIflow(id) }],
+					content: [{ type: "text", text: JSON.stringify({type: "success", iflowContent: escapedFileContent}) }],
 				};
 			} catch (error) {
 				logError(error);
@@ -33,7 +41,7 @@ export const registerIflowHandlers = (server: McpServer) => {
 					content: [
 						{
 							type: "text",
-							text: `Error: ${error}`,
+							text: JSON.stringify({type: "error", error}),
 						},
 					],
 					isError: true,
@@ -66,7 +74,7 @@ export const registerIflowHandlers = (server: McpServer) => {
 					content: [
 						{
 							type: "text",
-							text: `Error: ${error}`,
+							text: JSON.stringify({type: "error", error}),
 						},
 					],
 					isError: true,
@@ -79,11 +87,12 @@ export const registerIflowHandlers = (server: McpServer) => {
 		"update-iflow",
 		`Update or create files/content of an iflow
 		You only have to provide files that need to be updated
+		For file content use ${doublequotes} instead of just doublequotes otherwise you will get json parsing errors
         Folder structure is like this:
         src/main/resources/ is the root
-        src/mainresources/mapping contains message mappings in format <mappingname>.mmap with xml structure
-        src/mainresources/scripts contains groovy and javascript scripts that can be used within iflow
-        src/mainresources/scenarioflows/integrationflow/<iflow id>.iflw contains the iflow in xml structure
+        src/main/resources/mapping contains message mappings in format <mappingname>.mmap with xml structure
+        src/main/resources/scripts contains groovy and javascript scripts that can be used within iflow
+        src/main/resources/scenarioflows/integrationflow/<iflow id>.iflw contains the iflow in xml structure
         `,
 		{
 			id: z.string().describe("ID of the IFLOW"),
@@ -96,7 +105,12 @@ export const registerIflowHandlers = (server: McpServer) => {
 		},
 		async ({ id, files, autoDeploy }) => {
 			logInfo(`Updating iflow ${id} autodeploy: ${autoDeploy}`);
-			logInfo(files);
+
+			files.map(file => {
+				file.content = addDoublequotes(file.content);
+				return file;
+			});
+
 			try {
                 await updateIflow(id, files);
                 logInfo("Iflow updated successfully");

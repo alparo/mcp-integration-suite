4da433ce7d9ad1eb4a91a8d6d7d78335820b00e3
Noah Buchmann <noah.buchmann@contriboot.de>
2025-04-03 00:35:30 +0200

    add more JSDoc

--- src/utils/logging.ts
+++ src/utils/logging.ts
@@ -9,6 +9,10 @@ const log_file = fs.createWriteStream(path.resolve(projPath, "serverlog.txt"), {
 	mode: 0o666,
 });
 
-export const writeToLog = (d: any) =>  {
+/**
+ * Writes a log entry to the console
+ * @param d - The object or message to be logged
+ */
+export const writeToLog = (d: any) => {
 	log_file.write(util.format(d) + "\n");
 };
--- src/utils/zip.ts
+++ src/utils/zip.ts
@@ -4,8 +4,12 @@ import AdmZip from "adm-zip";
 import path from "node:path";
 import { rimraf } from "rimraf";
 
-
-
+/**
+ * Extracts a ZIP buffer to a specified folder
+ * @param zipBuf - The ZIP file as a buffer
+ * @param id - The unique identifier for the extraction folder
+ * @returns The path to the extracted folder
+ */
 export const extractToFolder = async (
 	zipBuf: Buffer,
 	id: string
@@ -20,6 +24,11 @@ export const extractToFolder = async (
 	return iflowPath;
 };
 
+/**
+ * Converts a folder to a ZIP buffer
+ * @param path - The path to the folder to be zipped
+ * @returns A buffer containing the zipped folder
+ */
 export const folderToZipBuffer = async (path: string): Promise<Buffer> => {
 	const zip = new AdmZip();
 	logInfo(`Adding ${path} to ZIP archive`);

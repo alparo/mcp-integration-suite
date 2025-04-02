8d422ba46ccf2b88048ec91fbf53b2237ea57e52
Noah Buchmann <noah.buchmann@contriboot.de>
2025-04-02 18:38:39 +0200

    handle wrong credentials with error

--- src/api/cpi_auth.ts
+++ src/api/cpi_auth.ts
@@ -1,4 +1,5 @@
 import { DestinationAuthToken } from "@sap-cloud-sdk/connectivity";
+import { logError } from "..";
 
 let tokenCache: {
 	token: DestinationAuthToken;
@@ -7,50 +8,56 @@ let tokenCache: {
 
 // TODO: handle CSRF
 export const getOAuthTokenCPI = async (): Promise<DestinationAuthToken> => {
-    // Check if token is expired
-    const now = Date.now();
-    if (tokenCache && tokenCache.expiresAt > now + 5 * 60 * 1000) {
-        return tokenCache.token;
-    }
-;
-    const params = new URLSearchParams();
-    params.append("grant_type", "client_credentials");
-    params.append("client_id", process.env.CPI_OAUTH_CLIENT_ID as string);
-    params.append("client_secret", process.env.CPI_OAUTH_CLIENT_SECRET as string);
+	// Check if token is expired
+	const now = Date.now();
+	if (tokenCache && tokenCache.expiresAt > now + 5 * 60 * 1000) {
+		return tokenCache.token;
+	}
+	const params = new URLSearchParams();
+	params.append("grant_type", "client_credentials");
+	params.append("client_id", process.env.CPI_OAUTH_CLIENT_ID as string);
+	params.append(
+		"client_secret",
+		process.env.CPI_OAUTH_CLIENT_SECRET as string
+	);
 
-    const response = await fetch(process.env.CPI_OAUTH_TOKEN_URL as string, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: params,
-    });
+	const response = await fetch(process.env.CPI_OAUTH_TOKEN_URL as string, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded",
+		},
+		body: params,
+	});
 
-    if (!response.ok) {
-        throw new Error(
-            `OAuth Token Fehler: ${response.status} ${response.statusText}`
-        );
-    }
+	if (response.status !== 200) {
+		throw new Error(
+			`Error getting OAuth token: ${response.status} ${response.statusText}`
+		);
+	}
 
-    const data = await response.json();
+	try {
+		const data = await response.json();
+		const token = {
+			value: data.access_token,
+			type: data.token_type,
+			expiresIn: data.expires_in,
+			http_header: {
+				key: "Authorization",
+				value: `Bearer ${data.access_token}`,
+			},
+			error: null,
+		};
 
-    // create token
-    const token = {
-        value: data.access_token,
-        type: data.token_type,
-        expiresIn: data.expires_in,
-        http_header: {
-            key: "Authorization",
-            value: `Bearer ${data.access_token}`,
-        },
-        error: null,
-    };
+		tokenCache = {
+			token,
+			expiresAt: now + data.expires_in * 1000,
+		};
 
-    // save token to cache
-    tokenCache = {
-        token,
-        expiresAt: now + data.expires_in * 1000,
-    };
-
-    return token;
+		return token;
+	} catch (error) {
+		logError(JSON.stringify(error));
+		throw new Error(
+			"Invalid response from OAuth authentication URL. Please consider checking your credentials/endpoints"
+		);
+	}
 };

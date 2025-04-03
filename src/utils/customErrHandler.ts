a8ff7ddce4885e89c7ef9592365bf0b587a8e427
Noah Buchmann <noah.buchmann@contriboot.de>
2025-04-03 22:45:12 +0200

    fix custom error handler

--- src/utils/customErrHandler.ts
+++ src/utils/customErrHandler.ts
@@ -4,20 +4,24 @@ import { logError } from "..";
 
 export const formatError = (error: any): contentReturnElement => {
 	logError(Object.keys(error));
-	logError(error);
+	//logError(JSON.stringify(error.cause.cause))
+	//logError(error);
 	if (error === null) {
 		return {
 			type: "text",
 			text: "Received a null error! This should never happen. Consider checking server logs",
 		};
 	}
-	if (axios.isAxiosError(error?.cause)) {
-		const axiosError = error.cause as AxiosError;
+	
+	if (axios.isAxiosError(error.cause?.cause)) {
+		logError('is error with cause')
+		const axiosError = error.cause.cause as AxiosError;
 		//here we have a type guard check, error inside this if will be treated as AxiosError
 		const response = axiosError?.response;
 		const request = axiosError?.request;
 
 		if (response) {
+			logError('is error with cause resp')
 			//The request was made and the server responded with a status code that falls out of the range of 2xx the http status code mentioned above
 			return {
 				type: "text",
@@ -25,7 +29,7 @@ export const formatError = (error: any): contentReturnElement => {
 					type: "response with error",
 					statusCode: response.status,
 					statusText: response.statusText,
-					responseBody: response.data,
+					responseBody: typeof response.data === 'string' ? response.data : 'undefined or binary',
 				}),
 			};
 		} else {
